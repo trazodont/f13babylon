026d43b674d9c4b2a91095444bb2b80f724f13f4
ZephyrTFA <matthew@tfaluc.com>
2023-11-10 01:36:40 -0500

    >:(

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -6,7 +6,8 @@ source dependencies.sh
 git clone https://github.com/tgstation/rust-g
 cd rust-g
 git checkout "$RUST_G_VERSION"
-cargo build --release
+rustup target add i686-unknown-linux-gnu
+cargo build --release --target i686-unknown-linux-gnu
 
 cp target/release/librust_g.so "$HOME/BYOND/bin/librust_g.so"
 ldd "$HOME/BYOND/bin/librust_g.so"
