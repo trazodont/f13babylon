b51d3784664d42c2ec1ba49a45eb2f6ae2712149
ZephyrTFA <matthew@tfaluc.com>
2023-11-10 01:40:21 -0500

    last fixup for rustg

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -6,7 +6,13 @@ source dependencies.sh
 git clone https://github.com/tgstation/rust-g
 cd rust-g
 git checkout "$RUST_G_VERSION"
+
+dpkg --add-architecture i386
+apt update
+apt install zlib1g-dev:i386
+
 rustup target add i686-unknown-linux-gnu
+export PKG_CONFIG_ALLOW_CROSS=1
 cargo build --release --target i686-unknown-linux-gnu
 
 cp target/release/librust_g.so "$HOME/BYOND/bin/librust_g.so"
