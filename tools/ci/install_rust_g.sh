69152d702ddf6e94dca5889ad056905352bde62b
ZephyrTFA <matthew@tfaluc.com>
2023-11-10 01:42:51 -0500

    yeah

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -7,9 +7,9 @@ git clone https://github.com/tgstation/rust-g
 cd rust-g
 git checkout "$RUST_G_VERSION"
 
-dpkg --add-architecture i386
-apt update
-apt install zlib1g-dev:i386
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install zlib1g-dev:i386
 
 rustup target add i686-unknown-linux-gnu
 export PKG_CONFIG_ALLOW_CROSS=1
