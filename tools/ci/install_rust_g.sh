54c7cc3d27372fb73b6526f334635fb08ebfde0d
ZephyrTFA <matthew@tfaluc.com>
2023-11-10 01:26:47 -0500

    fuck

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 source dependencies.sh
 
-mkdir -p" $HOME/BYOND/bin"
+mkdir -p "$HOME/BYOND/bin"
 wget -O "$HOME/BYOND/bin/librust_g.so" "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
 chmod +x "$HOME/BYOND/bin/librust_g.so"
 ldd "$HOME/BYOND/bin/librust_g.so"
