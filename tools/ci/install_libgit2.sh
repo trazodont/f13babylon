7031bcd7ab42cca3cc85ade26acaef7f493fca83
ZephyrTFA <matthew@tfaluc.com>
2023-11-09 23:11:36 -0500

    more updates, cache libgit dev

--- tools/ci/install_libgit2.sh
+++ tools/ci/install_libgit2.sh
@@ -5,8 +5,17 @@ apt install -y cmake unzip libssl-dev
 
 mkdir ~/libgit2
 cd ~/libgit2
+
+if [ -f libgit2.zip ]; then
+	rm libgit2.zip
+fi
 wget -O libgit2.zip "$source_url"
+
+if [ -d libgit2-* ]; then
+	rm -rf libgit2-*
+fi
 unzip libgit2.zip
+
 cd libgit2-*
 mkdir build
 cd build
